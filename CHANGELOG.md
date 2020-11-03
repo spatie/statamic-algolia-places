fe1a6cdae11648c4c25e2f156c08fd5a07f07e7f
Rias <hey@rias.be>
2020-11-03 11:45:11 +0100

    Use site

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,11 @@
 
 All notable changes to `statamic-algolia-places` will be documented in this file
 
+## 1.0.2 - 2020-11-03
+
+## What's new
+- The fieldtype now uses the selected site's locale by default
+
 ## 1.0.1 - 2020-10-23
 
 ## What's fixed
--- resources/js/components/places-field.vue
+++ resources/js/components/places-field.vue
@@ -1,102 +1,116 @@
 <template>
-    <div>
-        <div
-            class="bg-red-lighter p-3 text-sm text-red"
-            v-if="error.key || error.id"
-        >
-            <span v-html="__('Be sure to add the following environment variables to your .env')"></span>
-            <ul class="mt-1 list">
-                <li v-if="error.key">
-                    <code>PLACES_API_KEY</code>
-                </li>
-                <li v-if="error.id">
-                    <code>PLACES_APP_ID</code>
-                </li>
-            </ul>
-        </div>
-        <input
-            v-else
-            :placeholder="config.placeholder"
-            class="input-text"
-            ref="inputRef"
-            :value="inputValue"
-        />
+  <div>
+    <div
+        class="bg-red-lighter p-3 text-sm text-red"
+        v-if="error.key || error.id"
+    >
+      <span v-html="__('Be sure to add the following environment variables to your .env')"></span>
+      <ul class="mt-1 list">
+        <li v-if="error.key">
+          <code>PLACES_API_KEY</code>
+        </li>
+        <li v-if="error.id">
+          <code>PLACES_APP_ID</code>
+        </li>
+      </ul>
     </div>
+    <input
+        v-else
+        :placeholder="config.placeholder"
+        class="input-text"
+        ref="inputRef"
+        :value="inputValue"
+    />
+  </div>
 </template>
 
 <script>
 import places from "places.js";
 
 export default {
-    mixins: [Fieldtype],
-    data() {
-        return {
-            error: {
-                key: !this.meta.placesApiKey,
-                id: !this.meta.placesAppId
-            }
-        };
-    },
-    computed: {
-        inputValue() {
-            return this.value ? this.value.value : '';
-        }
-    },
-    methods: {
-        handleAddressSelect(e) {
-            delete e.suggestion.rawAnswer;
-            delete e.suggestion.hit;
-            delete e.suggestion.hitIndex;
-            delete e.suggestion.highlight;
-
-            this.update(e.suggestion);
-        }
-    },
-    mounted() {
-        const { error } = this;
-        const { placesApiKey, placesAppId } = this.meta;
-        if (error.key || error.id) {
-            return;
-        } else {
-            const options = {
-                appId: placesAppId,
-                apiKey: placesApiKey,
-                container: this.$refs.inputRef,
-                postcodeSearch: !!this.config.postcodeSearch,
-                getRankingInfo: !!this.config.getRankingInfo,
-                useDeviceLocation: !!this.config.useDeviceLocation,
-            };
-
-            if (this.config.searchType !== 'all') {
-                options.type = this.config.searchType;
-            }
-
-            if (this.config.language) {
-                options.language = this.config.language;
-            }
-
-            if (this.config.countries) {
-                options.countries = this.config.countries;
-            }
-
-            switch (this.config.locationSearch) {
-                case 'lat_lng':
-                    options.aroundLatLng = this.config.aroundLatLng;
-                    break;
-                case 'lat_lng_ip':
-                    options.aroundLatLngViaIP = true;
-                    break;
-                case 'bounding_box':
-                    options.insideBoundingBox = this.config.insideBoundingBox;
-                    break;
-                case 'polygon':
-                    options.insidePolygon = this.config.insidePolygon;
-                    break;
-            }
-
-            const placesInstance = places(options);
-            placesInstance.on("change", this.handleAddressSelect);
-        }
+  mixins: [Fieldtype],
+  data() {
+    return {
+      error: {
+        key: !this.meta.placesApiKey,
+        id: !this.meta.placesAppId
+      }
+    };
+  },
+  computed: {
+    inputValue() {
+      return this.value ? this.value.value : '';
     }
+  },
+  methods: {
+    handleAddressSelect(e) {
+      delete e.suggestion.rawAnswer;
+      delete e.suggestion.hit;
+      delete e.suggestion.hitIndex;
+      delete e.suggestion.highlight;
+
+      this.update(e.suggestion);
+    }
+  },
+
+  inject: {
+    storeName: {
+      default: null
+    }
+  },
+
+  mounted() {
+    const {error} = this;
+    const {placesApiKey, placesAppId} = this.meta;
+    if (error.key || error.id) {
+      return;
+    } else {
+      let language = this.$config.get('selectedSite');
+
+      if (this.storeName) {
+        language = this.$store.state.publish[this.storeName].site;
+      }
+
+      if (this.config.language) {
+        language = this.config.language;
+      }
+
+      const options = {
+        appId: placesAppId,
+        apiKey: placesApiKey,
+        container: this.$refs.inputRef,
+        postcodeSearch: !!this.config.postcodeSearch,
+        getRankingInfo: !!this.config.getRankingInfo,
+        useDeviceLocation: !!this.config.useDeviceLocation,
+        language: language,
+      };
+
+      if (this.config.searchType !== 'all') {
+        options.type = this.config.searchType;
+      }
+
+      if (this.config.countries) {
+        options.countries = this.config.countries;
+      }
+
+      switch (this.config.locationSearch) {
+        case 'lat_lng':
+          options.aroundLatLng = this.config.aroundLatLng;
+          break;
+        case 'lat_lng_ip':
+          options.aroundLatLngViaIP = true;
+          break;
+        case 'bounding_box':
+          options.insideBoundingBox = this.config.insideBoundingBox;
+          break;
+        case 'polygon':
+          options.insidePolygon = this.config.insidePolygon;
+          break;
+      }
+
+      const placesInstance = places(options);
+      placesInstance.on("change", this.handleAddressSelect);
+    }
+  }
 };
 </script>
